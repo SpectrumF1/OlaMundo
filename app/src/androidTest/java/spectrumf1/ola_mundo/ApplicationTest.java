72955f21d0ccc6d3602128d35ea01577de624168
styti0n <joaocarvalho9@gmail.com>
2016-09-07 12:34:35 +0100

    Add Files

---
+++ app/src/androidTest/java/spectrumf1/ola_mundo/ApplicationTest.java
@@ -0,0 +1,13 @@
+package spectrumf1.ola_mundo;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
